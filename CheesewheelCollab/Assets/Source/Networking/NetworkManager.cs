9bffc54d61c704341a806547bcf3a7bea88a537c
Exanite <exanite78012@gmail.com>
2024-02-20 02:05:33 -0500

    Add audio packet channel with placeholder data

--- CheesewheelCollab/Assets/Source/Networking/NetworkManager.cs
+++ CheesewheelCollab/Assets/Source/Networking/NetworkManager.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Exanite.Networking;
 using Exanite.Networking.Channels;
+using LiteNetLib.Utils;
 using UniDi;
 using UnityEngine;
 using Network = Exanite.Networking.Network;
 
 namespace Source.Networking
 {
+    public class AudioPacket : INetworkSerializable
+    {
+        public float[] Data = new float[250];
+
+        public void Serialize(NetDataWriter writer)
+        {
+            for (var i = 0; i < Data.Length; i++)
+            {
+                writer.Put(Data[i]);
+            }
+        }
+
+        public void Deserialize(NetDataReader reader)
+        {
+            for (var i = 0; i < Data.Length; i++)
+            {
+                Data[i] = reader.GetFloat();
+            }
+        }
+    }
+
     public class NetworkManager : MonoBehaviour
     {
         [Inject] private IEnumerable<IPacketHandler> packetHandlers;
         [Inject] private Network coreNetwork;
         [Inject] private IChanneledNetwork network;
+
+        private INetworkChannel<AudioPacket> audioPacketChannel;
 
         private void Start()
         {
@@ -31,7 +56,31 @@ namespace Source.Networking
                 Debug.Log($"{(network.IsServer ? "Server" : "Client")} disconnected");
             };
 
+            audioPacketChannel = network.CreateChannel<AudioPacket>("AudioPacket", SendType.Unreliable, OnAudioPacket);
+
             coreNetwork.StartConnection().Forget();
+        }
+
+        private void Update()
+        {
+            if (network.IsClient)
+            {
+                for (var i = 0; i < audioPacketChannel.Message.Data.Length; i++)
+                {
+                    audioPacketChannel.Message.Data[i] = Time.time;
+                }
+
+                audioPacketChannel.Write();
+                foreach (var connection in network.Connections)
+                {
+                    audioPacketChannel.Send(connection);
+                }
+            }
+        }
+
+        private void OnAudioPacket(NetworkConnection connection, AudioPacket message)
+        {
+            Debug.Log($"Received on {connection.Network.GetType().Name}: {message.Data[0]}");
         }
     }
 }
