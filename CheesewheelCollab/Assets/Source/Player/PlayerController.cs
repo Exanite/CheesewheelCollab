be9ad1d829406929cc3b1879e1397f3108fc917e
Exanite <exanite78012@gmail.com>
2024-02-22 23:03:31 -0500

    Implement movement

--- CheesewheelCollab/Assets/Source/Player/PlayerController.cs
+++ CheesewheelCollab/Assets/Source/Player/PlayerController.cs
@@ -1,16 +1,31 @@
-using System;
-using Exanite.Core.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Source.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [Header("Dependencies")]
         [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private InputActionReference movement;
+
+        [Header("Basic")]
+        [SerializeField] private float movementSpeed = 8f;
+        [SerializeField] private float movementSpeedSmoothTime = 0.03f;
+
+        private Vector2 velocitySmoothing;
 
         private void Update()
         {
+            // Input
+            var movementInput = movement.action.ReadValue<Vector2>();
 
+            // Modify
+            var targetVelocity = Vector2.ClampMagnitude(movementInput, 1);
+            targetVelocity *= movementSpeed;
+
+            // Apply
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref velocitySmoothing, movementSpeedSmoothTime);
         }
     }
 }
