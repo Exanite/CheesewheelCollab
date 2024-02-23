dc5a648719e559b6b2f1d16d8eee5f66a0b624b0
Exanite <exanite78012@gmail.com>
2024-02-22 22:51:39 -0500

    Add empty PlayerController

---
+++ CheesewheelCollab/Assets/Source/Player/PlayerController.cs
@@ -0,0 +1,16 @@
+using System;
+using Exanite.Core.Collections;
+using UnityEngine;
+
+namespace Source.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rb;
+
+        private void Update()
+        {
+
+        }
+    }
+}
