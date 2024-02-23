fb241fd3488aac9a28a98b453391cbad7cc7155d
Exanite <exanite78012@gmail.com>
2024-02-23 09:35:43 -0500

    Remove unnecessary copy

--- CheesewheelCollab/Assets/Source/Audio/AudioRecorder.cs
+++ CheesewheelCollab/Assets/Source/Audio/AudioRecorder.cs
@@ -34,7 +34,6 @@ namespace Source.Audio
                 {
                     recording.GetData(Buffer, lastPosition + i * Buffer.Length);
 
-                    Buffer.CopyTo(Buffer.AsSpan());
                     SendSamples();
                 }
 
