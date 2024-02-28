acdb6f09cf529a829c7af425dd8d89966fbe8de5
Exanite <exanite78012@gmail.com>
2024-02-28 14:49:30 -0500

    Implement recording device listing

--- CheesewheelCollab/Assets/Source/Audio/AudioRecorder2.cs
+++ CheesewheelCollab/Assets/Source/Audio/AudioRecorder2.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Exanite.Core.Utilities;
 using SDL2;
 using UnityEngine;
 
@@ -7,19 +9,33 @@ namespace Source.Audio
     {
         private void Start()
         {
+            SDL.SDL_Init(SDL.SDL_INIT_AUDIO);
             var requestedSpec = new SDL.SDL_AudioSpec
             {
                 freq = 44100,
                 format = SDL.AUDIO_F32,
                 channels = 1,
-                samples = 10000,
+                samples = 250,
                 callback = (userdata, stream, len) =>
                 {
-
+                    "Test".Dump();
                 },
             };
 
-            SDL.SDL_OpenAudioDevice("", (int)SDL.SDL_bool.SDL_TRUE, ref requestedSpec, out var actualSpec, 0);
+            var deviceNames = new List<string>();
+            var deviceCount = SDL.SDL_GetNumAudioDevices(1);
+            for (var i = 0; i < deviceCount; i++)
+            {
+                deviceNames.Add(SDL.SDL_GetAudioDeviceName(i, 1));
+            }
+
+            Debug.Log($"Available recording devices: {DebugUtility.Format(deviceNames)}");
+
+            var deviceId = (int)SDL.SDL_OpenAudioDevice("", 1, ref requestedSpec, out var actualSpec, 0);
+            var deviceName = SDL.SDL_GetAudioDeviceName(deviceId, 1);
+            Debug.Log($"Recording using {deviceName}");
+
+            actualSpec.size.Dump();
         }
     }
 }
