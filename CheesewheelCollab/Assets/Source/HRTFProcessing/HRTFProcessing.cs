ff160f1c8c62b9a983275463dc5778f3d6daa569
pixelfac <n.harris1@ufl.edu>
2024-03-10 22:41:07 -0400

    add source comments

--- CheesewheelCollab/Assets/Source/HRTFProcessing/HRTFProcessing.cs
+++ CheesewheelCollab/Assets/Source/HRTFProcessing/HRTFProcessing.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/*
+ * This function was taken from the Accord Framework, under LGPL License
+ * https://github.com/accord-net/framework/blob/1ab0cc0ba55bcc3d46f20e7bbe7224b58cd01854/Sources/Accord.Math/Matrix/Matrix.Common.cs#L1937
+ */
 public static class HRTFProcessing
 {
     public static double[] Convolve(this double[] a, double[] kernel, bool trim)
