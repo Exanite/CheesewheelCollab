e098fa1acc9fefce74f3111db0792a59bcb01a9f
Exanite <exanite78012@gmail.com>
2024-03-19 14:22:46 -0400

    Fix incorrect sample rate (I thought I changed this, what?)

--- CheesewheelCollab/Assets/Source/Audio/AudioConstants.cs
+++ CheesewheelCollab/Assets/Source/Audio/AudioConstants.cs
@@ -2,7 +2,7 @@ namespace Source.Audio
 {
     public static class AudioConstants
     {
-        public const int SampleRate = 10000;
+        public const int SampleRate = 44100;
 
         /// <summary>
         /// The number of float32 samples in each chunk of samples. This should be used everywhere in the project.
