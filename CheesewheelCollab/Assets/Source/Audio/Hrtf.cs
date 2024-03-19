d1628945a5f5d39d8acb802cf83bd34768743c0f
Exanite <exanite78012@gmail.com>
2024-03-19 16:23:57 -0400

    Add the attribution comment back

--- CheesewheelCollab/Assets/Source/Audio/Hrtf.cs
+++ CheesewheelCollab/Assets/Source/Audio/Hrtf.cs
@@ -114,6 +114,11 @@ namespace Source.Audio
         }
 
         private float[] convolveResult = new float[AudioConstants.SamplesChunkSize];
+
+        /// <remarks>
+        /// This function was taken from the Accord Framework, under the LGPL License.
+        /// https://github.com/accord-net/framework/blob/1ab0cc0ba55bcc3d46f20e7bbe7224b58cd01854/Sources/Accord.Math/Matrix/Matrix.Common.cs#L1937
+        /// </remarks>
         public float[] Convolve(float[] samples, float[] hrtf)
         {
             var m = (int)Math.Ceiling(hrtf.Length / 2.0);
