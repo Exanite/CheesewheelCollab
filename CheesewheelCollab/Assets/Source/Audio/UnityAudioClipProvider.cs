024263475a4092d96b14a199c0d6939d88da5f7b
Exanite <exanite78012@gmail.com>
2024-03-19 14:04:39 -0400

    Add UnityAudioClipProvider

---
+++ CheesewheelCollab/Assets/Source/Audio/UnityAudioClipProvider.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Source.Audio
+{
+    public class UnityAudioClipProvider : AudioProvider
+    {
+        [SerializeField] private AudioClip clip;
+
+        private int chunk;
+        private float elapsedTime;
+        private float[] samples;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            var length = clip.samples;
+            samples = new float[length];
+            clip.GetData(samples, 0);
+        }
+
+        protected override void Update()
+        {
+            var stepTime = (float)AudioConstants.SamplesChunkSize / AudioConstants.SampleRate;
+
+            elapsedTime += Time.deltaTime;
+            while (elapsedTime >= stepTime)
+            {
+                var startPosition = chunk * AudioConstants.SamplesChunkSize;
+                for (var i = 0; i < AudioConstants.SamplesChunkSize; i++)
+                {
+                    Buffer[i] = samples[(startPosition + i) % samples.Length];
+                }
+
+                OnSamplesAvailable(chunk, Buffer);
+
+                elapsedTime -= stepTime;
+                chunk++;
+            }
+
+            base.Update();
+        }
+    }
+}
