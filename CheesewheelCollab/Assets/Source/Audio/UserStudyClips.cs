be6ffe61464d9f9d25e2e220bc771511378e5685
pixelfac <n.harris1@ufl.edu>
2024-04-10 21:48:34 -0400

    create coroutine helper funcs for clips

--- CheesewheelCollab/Assets/Source/Audio/UserStudyClips.cs
+++ CheesewheelCollab/Assets/Source/Audio/UserStudyClips.cs
@@ -8,20 +8,24 @@ namespace Source.Audio
         [Header("Required")]
         [SerializeField] private GameObject audioSourcePrefab;
         [SerializeField] private AudioClip[] clips;
+        public enum Clips { BUSINESS, MOMENT, DISCORD, BELL, GLOCK, KNOCKING, FOOTSTEPS };
+
 
 		private void Start()
 		{
-            ClipTest();
-            Invoke("ClipTest", 3);
+            StartCoroutine(CreateClip(Clips.DISCORD, 0, 0, 1));
+            StartCoroutine(CreateClip(Clips.BELL, 1, 1, 0));
 		}
 
-		public void ClipTest()
+		public IEnumerator CreateClip(Clips clip, float delay, float xPos, float yPos)
 		{
+            yield return new WaitForSeconds(delay);
+
             //play several sounds, at a delay
-            GameObject instance = Instantiate(audioSourcePrefab);
+            GameObject instance = Instantiate(audioSourcePrefab, new Vector3(xPos, yPos, 0), Quaternion.identity);
             CustomAudioSource cas = instance.GetComponent<CustomAudioSource>();
             cas.Loop = false;
-            cas.Clip = clips[0];
+            cas.Clip = clips[(int)clip];
 		}
     }
 }
