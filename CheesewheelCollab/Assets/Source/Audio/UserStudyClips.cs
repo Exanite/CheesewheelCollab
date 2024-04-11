9ca11f62b14a27638e6b2af5436e5ea1b9495500
pixelfac <n.harris1@ufl.edu>
2024-04-10 23:43:33 -0400

    add a bunch of sound sequences

--- CheesewheelCollab/Assets/Source/Audio/UserStudyClips.cs
+++ CheesewheelCollab/Assets/Source/Audio/UserStudyClips.cs
@@ -14,18 +14,118 @@ namespace Source.Audio
 
 		private void Start()
 		{
-            Clip1();
+            Clip6();
 		}
 
-        public void Clip1()
+        public void Clip0() // no notif from all directions
+        {
+            StartCoroutine(CreateClip(Clips.MOMENT, 0, 0, 1));
+
+            StartCoroutine(CreateClip(Clips.MOMENT, 5, 0, -1));
+
+            StartCoroutine(CreateClip(Clips.MOMENT, 5, 1, 0));
+
+            StartCoroutine(CreateClip(Clips.MOMENT, 5, -1, 0));
+        }
+
+        public void Clip1() // discord ping from all directions
 		{
             StartCoroutine(CreateClip(Clips.DISCORD, 0, 0, 1));
             StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, 1));
+
+            StartCoroutine(CreateClip(Clips.DISCORD, 5, 0, -1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, -1));
+
+            StartCoroutine(CreateClip(Clips.DISCORD, 5, 1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 1, 0));
+
             StartCoroutine(CreateClip(Clips.DISCORD, 5, -1, 0));
             StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
         }
 
-		public IEnumerator CreateClip(Clips clip, float delay, float xPos, float yPos)
+        public void Clip2() // bell ping from all directions
+        {
+            StartCoroutine(CreateClip(Clips.BELL, 0, 0, 1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, 1));
+
+            StartCoroutine(CreateClip(Clips.BELL, 5, 0, -1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, -1));
+
+            StartCoroutine(CreateClip(Clips.BELL, 5, 1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 1, 0));
+
+            StartCoroutine(CreateClip(Clips.BELL, 5, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+        }
+
+        public void Clip3() // glock ping from all directions
+        {
+            StartCoroutine(CreateClip(Clips.GLOCK, 0, 0, 1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, 1));
+
+            StartCoroutine(CreateClip(Clips.GLOCK, 5, 0, -1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, -1));
+
+            StartCoroutine(CreateClip(Clips.GLOCK, 5, 1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 1, 0));
+
+            StartCoroutine(CreateClip(Clips.GLOCK, 5, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+        }
+
+        public void Clip4() // KNOCKING ping from all directions
+        {
+            StartCoroutine(CreateClip(Clips.KNOCKING, 0, 0, 1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, 1));
+
+            StartCoroutine(CreateClip(Clips.KNOCKING, 5, 0, -1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, -1));
+
+            StartCoroutine(CreateClip(Clips.KNOCKING, 5, 1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 1, 0));
+
+            StartCoroutine(CreateClip(Clips.KNOCKING, 5, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+        }
+
+        public void Clip5() // FOOTSTEPS ping from all directions
+        {
+            StartCoroutine(CreateClip(Clips.FOOTSTEPS, 0, 0, 1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, 1));
+
+            StartCoroutine(CreateClip(Clips.FOOTSTEPS, 5, 0, -1));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 0, -1));
+
+            StartCoroutine(CreateClip(Clips.FOOTSTEPS, 5, 1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, 1, 0));
+
+            StartCoroutine(CreateClip(Clips.FOOTSTEPS, 5, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+        }
+
+        public void Clip6() // interruption test
+        {
+            StartCoroutine(CreateClip(Clips.BUSINESS, 0, 1, 2));
+
+            StartCoroutine(CreateClip(Clips.MOMENT, 5, -1, 0));
+
+            StartCoroutine(CreateClip(Clips.DISCORD, 6, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+
+            StartCoroutine(CreateClip(Clips.BELL, 6, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+
+            StartCoroutine(CreateClip(Clips.GLOCK, 6, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+
+            StartCoroutine(CreateClip(Clips.KNOCKING, 6, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+
+            StartCoroutine(CreateClip(Clips.FOOTSTEPS, 6, -1, 0));
+            StartCoroutine(CreateClip(Clips.MOMENT, 0.5f, -1, 0));
+        }
+
+        public IEnumerator CreateClip(Clips clip, float delay, float xPos, float yPos)
 		{
             globalDelay += delay;
             yield return new WaitForSeconds(globalDelay);
